dc8184708f06df849a82c7685315e0bc79cc91e1
alexchiu00 <alexchiu.t@gmail.com>
2022-07-07 22:23:47 +0800

    set up error handler class & user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signIn";
 import { signoutRouter } from "./routes/signOut";
 import { signupRouter } from "./routes/signUp";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -13,6 +18,23 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.listen(3000, () => {
-  console.log("Listening to port 3000");
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening to port 3000");
+  });
+};
+
+start();
