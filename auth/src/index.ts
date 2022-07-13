12be5e9e6c93206baa33f84009227411dfc29eed
alexchiu00 <alexchiu.t@gmail.com>
2022-07-13 16:17:30 +0800

    add auth test setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,28 +1,5 @@
-import express from "express";
-import "express-async-errors";
 import mongoose from "mongoose";
-
-import { currentUserRouter } from "./routes/currentUser";
-import { signinRouter } from "./routes/signIn";
-import { signoutRouter } from "./routes/signOut";
-import { signupRouter } from "./routes/signUp";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-
-app.use(express.json());
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -1,9 +1,10 @@
+import { currentUser } from './../middlewares/current-user';
 import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("hey");
+router.get("/api/users/currentuser", currentUser,(req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
